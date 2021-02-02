a40240d62bc3d50e2892e638e6cf66fb623cb70d
Alignof <neuromancer_wg@outlook.jp>
2021-02-02 22:38:09 +0900

    [update] colored output

--- gui_display.py
+++ gui_display.py
@@ -8,32 +8,36 @@ def gui_setup():
 
 	# window theme
 	sg.theme('DarkGrey13')
+	MLINE_KEY = '-ML-'+sg.WRITE_ONLY_KEY   # multiline element's key. Indicate it's an output only element
 
 	# Define the window's contents
-	layout = [	[sg.Output(size=(150,25), key='-OUTPUT-')],
+	layout = [	[sg.Multiline(size=(150,25), key=MLINE_KEY)],
 				[sg.Input(size=(150,1), key='-INPUT-')],
 				[sg.Button('Ok', bind_return_key=True), sg.Button('Quit')]]
 
 	# Create the window
 	window = sg.Window('Terminal-term', layout, resizable=True, size=(800,500))
 
+	# set display color
+	output_key = MLINE_KEY
+	sg.cprint_set_output_destination(window, output_key)
+
 def gui_get_command():
 	event, values = window.read()
-	window['-INPUT-']('')
+	sg.cprint(">>>  " + values['-INPUT-'], text_color="green")
 
 	# See if user wants to quit or window was closed
-	if event == sg.WINDOW_CLOSED or event == 'Quit':
+	if values['-INPUT-'].strip() == "exit" or event == sg.WINDOW_CLOSED or event == 'Quit':
 		sys.exit(1)
 
+	window['-INPUT-']('')
 	return values['-INPUT-']
 
 def gui_display_result(result):
 	if result.returncode == 0 :
 		stdout = str(result.stdout, encoding='utf-8', errors='replace')
-		#window['-OUTPUT-'].update(stdout)
-		print(stdout)
+		sg.cprint(stdout, text_color="white")
 	else:
 		stderr = str(result.stderr, encoding='utf-8', errors='replace')
-		#window['-OUTPUT-'].update(stderr)
-		print(stderr)
+		sg.cprint(stderr, text_color="red")
 
